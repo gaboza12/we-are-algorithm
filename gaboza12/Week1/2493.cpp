675e0bc6654a8278f3fb13cd8a32109fe99b7420
gaboza12 <milkies97@naver.com>
2024-06-13 17:14:47 +0900

    feat: 2493 / 골드5 / https://www.acmicpc.net/problem/2493

---
+++ gaboza12/Week1/2493.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	int n, h;
+
+	cin >> n;
+	
+	stack<pair<int, int>> s; //¿Œµ¶Ω∫, ≥Ù¿Ã
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> h;
+		
+		while (!s.empty())
+		{
+			if (h < s.top().second)
+			{
+				cout << s.top().first << " ";
+				break;
+			}
+			s.pop();
+		}
+
+		if (s.empty())
+		{
+			cout << "0 ";
+		}
+		
+		s.push({ i + 1, h });
+	}
+
+	return 0;
+}
