b2ca7b98bb4ef9bd8eeba0302afc7391072f2011
jake <wotjs2923@gmail.com>
2024-07-18 15:15:26 +0900

    feat: 2212 / G5

---
+++ 10o0o/Week6/2212.cpp
@@ -0,0 +1,28 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+#define FASTIO                 \
+  ios::sync_with_stdio(false); \
+  cin.tie(NULL);               \
+  cout.tie(NULL);
+
+#define F(i, a, b) for (int i = a; i < b; i += 1)
+#define FR(i, b, a) for (int i = b; i >= a; i -= 1)
+
+int n, k;
+
+int main() {
+  FASTIO
+  cin >> n >> k;
+  int dists[n], diffs[n - 1];
+  F(i, 0, n) cin >> dists[i];
+  sort(dists, dists + n);
+  F(i, 1, n) diffs[i - 1] = dists[i] - dists[i - 1];
+  sort(diffs, diffs + n - 1, greater<int>());
+  int result = dists[n - 1] - dists[0];
+  F(i, 0, min(n - 1, k - 1)) result -= diffs[i];
+  cout << result;
+  return 0;
+}
