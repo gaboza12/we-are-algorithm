02dabdc3d07990c86155e121d4ca0b22a8da5e55
gaboza12 <milkies97@naver.com>
2024-07-20 15:13:42 +0900

    feat: 2212 / 골드5 / https://www.acmicpc.net/problem/2212

---
+++ gaboza12/Week6/2212.cpp
@@ -0,0 +1,44 @@
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+
+int n, k;
+int ans = 0;
+
+vector<int> v, dist;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	cin >> n >> k;
+
+	for (int i = 0; i < n; i++)
+	{
+		int temp;
+		cin >> temp;
+		v.push_back(temp);
+	}
+
+	sort(v.begin(), v.end());
+
+	//센서 사이의 거리
+	for (int i = 0; i < n - 1; i++)
+	{
+		dist.push_back(v[i + 1] - v[i]);
+	}
+
+	//크기가 큰순서대로
+	sort(dist.begin(), dist.end(), greater<int>());
+
+	//큰 순서부터 k-1개를 제외하면 최소거리(k개의 집중국을 세우면 집중국 사이의 빈공간은 k-1개)
+	for (int i = k - 1; i < dist.size(); i++)
+	{
+		ans += dist[i];
+	}
+
+	cout << ans;
+
+	return 0;
+}
