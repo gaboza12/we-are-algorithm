e91ef4581009c88b71766fb67b161c12a0704531
gaboza12 <milkies97@naver.com>
2024-08-17 16:31:52 +0900

    feat : 트리순회 / G4 / https://www.acmicpc.net/problem/22856

---
+++ gaboza12/Week10/22856.cpp
@@ -0,0 +1,81 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+vector<int> graph[100005];
+
+bool flag = true;
+
+int end_node;
+int ans = 0;
+
+void find_end(int x)
+{
+	int left_node = graph[x][0];
+	int right_node = graph[x][1];
+
+	if (left_node != -1)
+		find_end(left_node);
+
+	end_node = x;
+
+	if (right_node != -1)
+		find_end(right_node);
+}
+
+void func(int x)
+{
+	int left_node = graph[x][0];
+	int right_node = graph[x][1];
+
+	if (left_node != -1)
+	{
+		ans++;
+		func(left_node);
+
+		if (flag)
+			ans++;
+	}
+
+	if (right_node != -1)
+	{
+		ans++;
+		func(right_node);
+
+		if (flag)
+			ans++;
+	}
+
+	if (x == end_node)
+	{
+		flag = false;
+		return;
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		int a, b, c;
+
+		cin >> a >> b >> c;
+		graph[a].push_back(b);
+		graph[a].push_back(c);
+	}
+
+	find_end(1);
+
+	func(1);
+
+	cout << ans;
+
+	return 0;
+}
