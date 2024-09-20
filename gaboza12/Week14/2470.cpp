c1e8f89196194f9df91db005ad06d7d81635bd40
gaboza12 <milkies97@naver.com>
2024-09-20 19:45:40 +0900

    feat: 2470 / 골드5 / https://www.acmicpc.net/problem/2470

---
+++ gaboza12/Week14/2470.cpp
@@ -0,0 +1,54 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+vector<int> v;
+
+int ans1 = 0, ans2 = 0;
+int num = 2000000000;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		int temp;
+		cin >> temp;
+		v.push_back(temp);
+	}
+
+	sort(v.begin(), v.end());
+
+	int st = 0, en = n - 1;
+
+	while (st < en)
+	{
+		int sum = v[st] + v[en];
+
+		if (abs(sum) < num)
+		{
+			num = abs(sum);
+
+			ans1 = v[st];
+			ans2 = v[en];
+
+			if (sum == 0)
+				break;
+		}
+
+		if (sum < 0)
+			st++;
+		else
+			en--;
+	}
+
+	cout << ans1 << " " << ans2;
+
+	return 0;
+}
