8a696493c13874402563a579714bf6bfb867a87e
gaboza12 <milkies97@naver.com>
2024-09-21 16:20:26 +0900

    feat: 6443 / 골드5 / https://www.acmicpc.net/problem/6443

---
+++ gaboza12/Week15/6443.cpp
@@ -0,0 +1,43 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+vector<set<string>> vs;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		string temp;
+
+		cin >> temp;
+
+		sort(temp.begin(), temp.end());
+
+		set<string> s;
+
+		do
+		{
+			s.insert(temp);
+		} while (next_permutation(temp.begin(), temp.end()));
+
+		vs.push_back(s);
+	}
+
+	for (int i = 0; i < vs.size(); i++)
+	{
+		for (auto nxt : vs[i])
+		{
+			cout << nxt << "\n";
+		}
+	}
+
+	return 0;
+}
