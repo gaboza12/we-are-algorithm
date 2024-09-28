49f0d4242702636e522af748b6910cf921a947f2
gaboza12 <milkies97@naver.com>
2024-09-28 14:35:51 +0900

    feat: 4256 / 골드2 / https://www.acmicpc.net/problem/4256

---
+++ gaboza12/Week16/4256.cpp
@@ -0,0 +1,58 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+int t, n;
+
+vector<int> v_pre, v_in;
+
+void post_order(int start, int end, int pos)
+{
+	for (int i = start; i < end; i++)
+	{
+		if (v_in[i] != v_pre[pos])
+			continue;
+
+		post_order(start, i, pos + 1);
+		post_order(i + 1, end, pos + 1 + i - start);
+		cout << v_pre[pos] << " ";
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	
+	cin >> t;
+
+	while (t--)
+	{
+		while (!v_pre.empty())
+			v_pre.pop_back();
+
+		while (!v_in.empty())
+			v_in.pop_back();
+
+		cin >> n;
+
+		for (int i = 0; i < n; i++)
+		{
+			int temp;
+			cin >> temp;
+			v_pre.push_back(temp);
+		}
+
+		for (int i = 0; i < n; i++)
+		{
+			int temp;
+			cin >> temp;
+			v_in.push_back(temp);
+		}
+
+		post_order(0, n, 0);
+		cout << "\n";
+	}
+
+	return 0;
+}
