16a0defe842862699fb52da31db5f699d67aeff5
gaboza12 <milkies97@naver.com>
2024-10-06 21:53:28 +0900

    feat: 1749 / 골드4 / https://www.acmicpc.net/problem/1749

---
+++ gaboza12/Week17/1749.cpp
@@ -0,0 +1,45 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+int board[205][205];
+int psum[205][205];
+
+int n, m;
+
+int ans = -1e9 - 10;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			cin >> board[i][j];
+			psum[i + 1][j + 1] = psum[i + 1][j] + psum[i][j + 1] - psum[i][j] + board[i][j];
+		}
+	}
+
+	for (int x1 = 1; x1 <= n; x1++)
+	{
+		for (int y1 = 1; y1 <= m; y1++)
+		{
+			for (int x2 = x1; x2 <= n; x2++)
+			{
+				for (int y2 = y1; y2 <= m; y2++)
+				{
+					ans = max(ans, psum[x2][y2] - psum[x1 - 1][y2] - psum[x2][y1 - 1] + psum[x1 - 1][y1 - 1]);
+				}
+			}
+		}
+	}
+
+	cout << ans;
+
+	return 0;
+}
