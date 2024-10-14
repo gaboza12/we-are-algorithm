6a6bd5d7832b9ac7270d43f2d0f5004b067a4fbd
gaboza12 <milkies97@naver.com>
2024-10-14 16:57:06 +0900

    feat: 17609 / 골드5 / https://www.acmicpc.net/problem/17609

---
+++ gaboza12/Week18/17609.cpp
@@ -0,0 +1,47 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+int t;
+
+int func(int left, int right, string str, bool flag)
+{
+	while (left < right)
+	{
+		if (str[left] != str[right])
+		{
+			if (!flag)
+			{
+				//유사 회문
+				if (func(left + 1, right, str, true) == 0 || func(left, right - 1, str, true) == 0)
+					return 1;
+			}
+			//회문, 유사회문 아님
+			return 2;
+		}
+
+		left++;
+		right--;
+	}
+	//while문 통과하면 회문
+	return 0;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	cin >> t;
+
+	for (int i = 0; i < t; i++)
+	{
+		string s;
+
+		cin >> s;
+
+		cout << func(0, s.size() - 1, s, false) << "\n";
+	}
+
+	return 0;
+}
