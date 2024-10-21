7b91531d44d14b04978723ffd3e7d5dc5462642a
gaboza12 <milkies97@naver.com>
2024-10-21 17:45:24 +0900

    feat: 10159 / 골드4 / https://www.acmicpc.net/problem/10159

---
+++ gaboza12/Week19/10159.cpp
@@ -0,0 +1,59 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, m;
+
+bool item[105][105];
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	cin >> n >> m;
+
+	for (int i = 0; i < 105; i++)
+	{
+		for (int j = 0; j < 105; j++)
+		{
+			if (i == j)
+				item[i][j] = true;
+		}
+	}
+
+	for (int i = 0; i < m; i++)
+	{
+		int a, b;
+
+		cin >> a >> b;
+
+		item[a][b] = true;
+	}
+
+	for (int k = 1; k <= n; k++)
+	{
+		for (int i = 1; i <= n; i++)
+		{
+			for (int j = 1; j <= n; j++)
+			{
+				item[i][j] = item[i][j] || (item[i][k] && item[k][j]);
+			}
+		}
+	}
+
+	for (int i = 1; i <= n; i++)
+	{
+		int cnt = 0;
+
+		for (int j = 1; j <= n; j++)
+		{
+			if (item[i][j] == false && item[j][i] == false)
+				cnt++;
+		}
+
+		cout << cnt << "\n";
+	}
+
+	return 0;
+}
