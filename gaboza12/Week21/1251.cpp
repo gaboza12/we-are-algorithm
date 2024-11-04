d53830d4671c85285b29deacba359b3afa7bb43f
gaboza12 <milkies97@naver.com>
2024-11-04 17:56:26 +0900

    feat: 1251 / 실버5 / https://www.acmicpc.net/problem/1251

---
+++ gaboza12/Week21/1251.cpp
@@ -0,0 +1,42 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+string s;
+vector<string> vec;
+string ans = "";
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	cin >> s;
+
+	for (int i = 1; i <= s.size() - 2; i++)
+	{
+		for (int j = 1; j <= s.size() - i - 1; j++)
+		{
+			string res = "";
+
+			string a = s.substr(0, i);
+			string b = s.substr(i, j);
+			string c = s.substr(i + j);
+
+			reverse(a.begin(), a.end());
+			reverse(b.begin(), b.end());
+			reverse(c.begin(), c.end());
+
+			res = a + b + c;
+
+			if (ans == "")
+				ans = res;
+			else if (ans > res)
+				ans = res;
+		}
+	}
+
+	cout << ans;
+
+	return 0;
+}
